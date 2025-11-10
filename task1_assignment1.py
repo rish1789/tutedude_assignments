6d4c5bf3b6ca9bf8e1b3b7e5e10437603b0e943e
rish1789 <108068479+rish1789@users.noreply.github.com>
2025-11-10 11:54:51 +0530

    ASSIGNMNET 1

---
+++ task1_assignment1.py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      risha
+#
+# Created:     10-11-2025
+# Copyright:   (c) risha 2025
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+num1 = int(input("Enter the First number:"))
+num2 = int(input("Enter the Second number:"))
+
+add = num1+num2
+mult = num1*num2
+sub = num1-num2
+div = round(num1/num2,1)
+
+print("Additon:",add)
+print("Subtraction:",sub)
+print("Multiplication :",mult)
+print("Division :",div)
+
---
+++ task2_assignment2.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      risha
+#
+# Created:     10-11-2025
+# Copyright:   (c) risha 2025
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+s1 = input("Enter the first name: ")
+s2 = input("Enter the last name: ")
+
+print("Hello, "+s1+" "+s2+"! Welcome to Python program. ")

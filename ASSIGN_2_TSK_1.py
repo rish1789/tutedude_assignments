4bfc22cd49b8b0fe95f2e9947a614dd3fbcb13c4
rish1789 <108068479+rish1789@users.noreply.github.com>
2025-11-24 20:08:28 +0530

    ASSIGNMENT 2
    ASSIGNMENT 2

---
+++ ASSIGN_2_TSK_1.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      risha
+#
+# Created:     24-11-2025
+# Copyright:   (c) risha 2025
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+num = int(input("Enter the number :"))
+
+print("EVEN") if num%2 == 0 else print("ODD")
---
+++ ASSIGN_2_TSK_2.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      risha
+#
+# Created:     24-11-2025
+# Copyright:   (c) risha 2025
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+sum=0
+
+for i in range (1,51):
+    sum += i
+
+print(f"The sum of numbers from 1 to 50 is {sum}")
